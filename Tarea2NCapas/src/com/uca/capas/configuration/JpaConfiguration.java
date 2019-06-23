009a4f74f09ef0d78c4ddd33da3e90fec93049e3
Ronald Vega <ronald.vega48@gmail.com>
2019-06-23 17:55:32 -0600

    Login ya funcionando

--- Tarea2NCapas/src/com/uca/capas/configuration/JpaConfiguration.java
+++ Tarea2NCapas/src/com/uca/capas/configuration/JpaConfiguration.java
@@ -18,6 +18,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "com.uca.capas.repository")
 public class JpaConfiguration {
 	@Bean
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
